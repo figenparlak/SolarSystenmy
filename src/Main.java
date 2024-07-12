f9f3d73ab2da726c1621094a2577980134ec8944
FigenParlak <figen.ayse.parlak@gmail.com>
2024-07-13 00:21:04 +0300

    issue in featureSun has been resolved and ready to merge to the FeatureSun

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         SUN  omaro = new SUN();
         omaro.temprature= "1236454kj";
         omaro.name= "ferro";
-        omaro.radius= 19383763738282929290;
+        omaro.radius= 1938;
     }
 
 
